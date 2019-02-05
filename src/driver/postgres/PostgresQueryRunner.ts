df1c230113fe4c7701bdf3b89bc28abe32cec774
atreidesend <jmeek13@gmail.com>
2019-02-05 12:08:34 +0000

    set and unset activeTransaction flag on databaseConnection
    LL specific change - our legacy services use this field to determine
    if a transaction is active or not, if we don't manage it via
    typeorm then it's possible legacy clients will still think they
    are in a transaction when they're not

--- src/driver/postgres/PostgresQueryRunner.ts
+++ src/driver/postgres/PostgresQueryRunner.ts
@@ -145,6 +145,10 @@ export class PostgresQueryRunner extends BaseQueryRunner implements QueryRunner 
 
         this.isTransactionActive = true;
         await this.query("START TRANSACTION");
+        // in remus, the LL postgres client checks if it's in a transaction
+        // using this field, if typeorm doesn't set/unset it we can wind up
+        // where legacy code thinks a transaction is active when it's really not
+        this.databaseConnection.activeTransaction = true;
         if (isolationLevel) {
             await this.query("SET TRANSACTION ISOLATION LEVEL " + isolationLevel);
         }
@@ -172,6 +176,11 @@ export class PostgresQueryRunner extends BaseQueryRunner implements QueryRunner 
         const afterBroadcastResult = new BroadcasterResult();
         this.broadcaster.broadcastAfterTransactionCommitEvent(afterBroadcastResult);
         if (afterBroadcastResult.promises.length > 0) await Promise.all(afterBroadcastResult.promises);
+
+        // in remus, the LL postgres client checks if it's in a transaction
+        // using this field, if typeorm doesn't set/unset it we can wind up
+        // where legacy code thinks a transaction is active when it's really not
+        this.databaseConnection.activeTransaction = false;
     }
 
     /**
@@ -192,6 +201,11 @@ export class PostgresQueryRunner extends BaseQueryRunner implements QueryRunner 
         const afterBroadcastResult = new BroadcasterResult();
         this.broadcaster.broadcastAfterTransactionRollbackEvent(afterBroadcastResult);
         if (afterBroadcastResult.promises.length > 0) await Promise.all(afterBroadcastResult.promises);
+
+        // in remus, the LL postgres client checks if it's in a transaction
+        // using this field, if typeorm doesn't set/unset it we can wind up
+        // where legacy code thinks a transaction is active when it's really not
+        this.databaseConnection.activeTransaction = false;
     }
 
     /**
@@ -219,7 +233,6 @@ export class PostgresQueryRunner extends BaseQueryRunner implements QueryRunner 
                 case "UPDATE":
                     // for UPDATE and DELETE query additionally return number of affected rows
                     return [result.rows, result.rowCount];
-                    break;
                 default:
                     return result.rows;
             }
